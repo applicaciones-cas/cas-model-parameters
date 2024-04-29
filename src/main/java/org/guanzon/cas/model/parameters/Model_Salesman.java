e5e5ab498cb725bc78bcf2958806cb691768bbe3
Baxic <WstActGitHub@gmail.com>
2024-04-29 02:54:59 -0400

    EJ - refactorModelSalesman
    Fix Format and Add makeSelectSQL

--- src/main/java/org/guanzon/cas/model/parameters/Model_Salesman.java
+++ src/main/java/org/guanzon/cas/model/parameters/Model_Salesman.java
@@ -14,52 +14,54 @@ import org.guanzon.appdriver.constant.EditMode;
 import org.guanzon.appdriver.iface.GEntity;
 import org.json.simple.JSONObject;
 
-
 /**
  * @author Michael Cuison
  */
-public class Model_Salesman implements GEntity{
+public class Model_Salesman implements GEntity {
+
     final String XML = "Model_Salesman.xml";
-    
+
     GRider poGRider;                //application driver
     CachedRowSet poEntity;          //rowset
     JSONObject poJSON;              //json container
     int pnEditMode;                 //edit mode
-    
+
     /**
      * Entity constructor
-     * 
+     *
      * @param foValue - GhostRider Application Driver
      */
-    public Model_Salesman(GRider foValue){
-        if (foValue == null){
+    public Model_Salesman(GRider foValue) {
+        if (foValue == null) {
             System.err.println("Application Driver is not set.");
             System.exit(1);
         }
-        
+
         poGRider = foValue;
-        
+
         initialize();
     }
-    
+
     /**
      * Gets edit mode of the record
+     *
      * @return edit mode
      */
     @Override
     public int getEditMode() {
         return pnEditMode;
     }
-    
+
     /**
      * Gets the column index name.
+     *
      * @param fnValue - column index number
      * @return column index name
      */
     @Override
     public String getColumn(int fnValue) {
         try {
-            return poEntity.getMetaData().getColumnLabel(fnValue); 
+            return poEntity.getMetaData().getColumnLabel(fnValue);
         } catch (SQLException e) {
         }
         return "";
@@ -67,6 +69,7 @@ public class Model_Salesman implements GEntity{
 
     /**
      * Gets the column index number.
+     *
      * @param fsValue - column index name
      * @return column index number
      */
@@ -82,31 +85,33 @@ public class Model_Salesman implements GEntity{
 
     /**
      * Gets the total number of column.
+     *
      * @return total number of column
      */
     @Override
     public int getColumnCount() {
         try {
-            return poEntity.getMetaData().getColumnCount(); 
+            return poEntity.getMetaData().getColumnCount();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        
+
         return -1;
     }
 
     /**
      * Gets the table name.
+     *
      * @return table name
      */
     @Override
     public String getTable() {
         return "Salesman";
     }
-    
+
     /**
      * Gets the value of a column index number.
-     * 
+     *
      * @param fnColumn - column index number
      * @return object value
      */
@@ -122,7 +127,7 @@ public class Model_Salesman implements GEntity{
 
     /**
      * Gets the value of a column index name.
-     * 
+     *
      * @param fsColumn - column index name
      * @return object value
      */
@@ -135,23 +140,25 @@ public class Model_Salesman implements GEntity{
         }
         return null;
     }
-    
+
     /**
      * Sets column value.
-     * 
+     *
      * @param fnColumn - column index number
      * @param foValue - value
      * @return result as success/failed
      */
     @Override
     public JSONObject setValue(int fnColumn, Object foValue) {
-        try {  
+        try {
             poJSON = MiscUtil.validateColumnValue(System.getProperty("sys.default.path.metadata") + XML, MiscUtil.getColumnLabel(poEntity, fnColumn), foValue);
-            if ("error".equals((String) poJSON.get("result"))) return poJSON;
-            
+            if ("error".equals((String) poJSON.get("result"))) {
+                return poJSON;
+            }
+
             poEntity.updateObject(fnColumn, foValue);
             poEntity.updateRow();
-            
+
             poJSON = new JSONObject();
             poJSON.put("result", "success");
             poJSON.put("value", getValue(fnColumn));
@@ -160,13 +167,13 @@ public class Model_Salesman implements GEntity{
             poJSON.put("result", "error");
             poJSON.put("message", e.getMessage());
         }
-        
+
         return poJSON;
     }
 
     /**
      * Sets column value.
-     * 
+     *
      * @param fsColumn - column index name
      * @param foValue - value
      * @return result as success/failed
@@ -174,7 +181,7 @@ public class Model_Salesman implements GEntity{
     @Override
     public JSONObject setValue(String fsColumn, Object foValue) {
         poJSON = new JSONObject();
-        
+
         try {
             return setValue(MiscUtil.getColumnIndex(poEntity, fsColumn), foValue);
         } catch (SQLException e) {
@@ -184,19 +191,19 @@ public class Model_Salesman implements GEntity{
         }
         return poJSON;
     }
-    
+
     /**
      * Set the edit mode of the entity to new.
-     * 
+     *
      * @return result as success/failed
      */
     @Override
     public JSONObject newRecord() {
         pnEditMode = EditMode.ADDNEW;
-        
+
         //replace with the primary key column info
         setEmployerID(MiscUtil.getNextCode(getTable(), "sEmployID", true, poGRider.getConnection(), poGRider.getBranchCode()));
-        
+
         poJSON = new JSONObject();
         poJSON.put("result", "success");
         return poJSON;
@@ -204,29 +211,29 @@ public class Model_Salesman implements GEntity{
 
     /**
      * Opens a record.
-     * 
+     *
      * @param fsCondition - filter values
      * @return result as success/failed
      */
     @Override
     public JSONObject openRecord(String fsCondition) {
         poJSON = new JSONObject();
-        
+
         String lsSQL = MiscUtil.makeSelect(this, "xClientNm»xBranchNm");
-        
+
         //replace the condition based on the primary key column of the record
-        lsSQL = MiscUtil.addCondition(lsSQL, fsCondition);
-        
+        lsSQL = MiscUtil.addCondition(lsSQL, "sEmployID = " + SQLUtil.toSQL(fsCondition));
+
         ResultSet loRS = poGRider.executeQuery(lsSQL);
-        
+
         try {
-            if (loRS.next()){
-                for (int lnCtr = 1; lnCtr <= loRS.getMetaData().getColumnCount(); lnCtr++){
+            if (loRS.next()) {
+                for (int lnCtr = 1; lnCtr <= loRS.getMetaData().getColumnCount(); lnCtr++) {
                     setValue(lnCtr, loRS.getObject(lnCtr));
                 }
-                
+
                 pnEditMode = EditMode.UPDATE;
-                
+
                 poJSON.put("result", "success");
                 poJSON.put("message", "Record loaded successfully.");
             } else {
@@ -237,29 +244,29 @@ public class Model_Salesman implements GEntity{
             poJSON.put("result", "error");
             poJSON.put("message", e.getMessage());
         }
-        
+
         return poJSON;
     }
 
     /**
      * Save the entity.
-     * 
+     *
      * @return result as success/failed
      */
     @Override
     public JSONObject saveRecord() {
         poJSON = new JSONObject();
-        
-        if (pnEditMode == EditMode.ADDNEW || pnEditMode == EditMode.UPDATE){
+
+        if (pnEditMode == EditMode.ADDNEW || pnEditMode == EditMode.UPDATE) {
             String lsSQL;
-            if (pnEditMode == EditMode.ADDNEW){
+            if (pnEditMode == EditMode.ADDNEW) {
                 //replace with the primary key column info
                 setEmployerID(MiscUtil.getNextCode(getTable(), "sEmployID", true, poGRider.getConnection(), poGRider.getBranchCode()));
-                
+
                 lsSQL = makeSQL();
-                
-                if (!lsSQL.isEmpty()){
-                    if (poGRider.executeQuery(lsSQL, getTable(), poGRider.getBranchCode(), "") > 0){
+
+                if (!lsSQL.isEmpty()) {
+                    if (poGRider.executeQuery(lsSQL, getTable(), poGRider.getBranchCode(), "") > 0) {
                         poJSON.put("result", "success");
                         poJSON.put("message", "Record saved successfully.");
                     } else {
@@ -272,16 +279,16 @@ public class Model_Salesman implements GEntity{
                 }
             } else {
                 Model_Salesman loOldEntity = new Model_Salesman(poGRider);
-                
+
                 //replace with the primary key column info
                 JSONObject loJSON = loOldEntity.openRecord(this.getEmployerID());
-                
-                if ("success".equals((String) loJSON.get("result"))){
+
+                if ("success".equals((String) loJSON.get("result"))) {
                     //replace the condition based on the primary key column of the record
                     lsSQL = MiscUtil.makeSQL(this, loOldEntity, "sEmployID = " + SQLUtil.toSQL(this.getEmployerID()), "xClientNm»xBranchNm");
-                    
-                    if (!lsSQL.isEmpty()){
-                        if (poGRider.executeQuery(lsSQL, getTable(), poGRider.getBranchCode(), "") > 0){
+
+                    if (!lsSQL.isEmpty()) {
+                        if (poGRider.executeQuery(lsSQL, getTable(), poGRider.getBranchCode(), "") > 0) {
                             poJSON.put("result", "success");
                             poJSON.put("message", "Record saved successfully.");
                         } else {
@@ -302,10 +309,10 @@ public class Model_Salesman implements GEntity{
             poJSON.put("message", "Invalid update mode. Unable to save record.");
             return poJSON;
         }
-        
+
         return poJSON;
     }
-    
+
     /**
      * Prints all the public methods used<br>
      * and prints the column names of this entity.
@@ -313,217 +320,226 @@ public class Model_Salesman implements GEntity{
     @Override
     public void list() {
         Method[] methods = this.getClass().getMethods();
-        
+
         System.out.println("--------------------------------------------------------------------");
         System.out.println("LIST OF PUBLIC METHODS FOR " + this.getClass().getName() + ":");
         System.out.println("--------------------------------------------------------------------");
         for (Method method : methods) {
             System.out.println(method.getName());
         }
-        
+
         try {
             int lnRow = poEntity.getMetaData().getColumnCount();
-        
+
             System.out.println("--------------------------------------------------------------------");
             System.out.println("ENTITY COLUMN INFO");
             System.out.println("--------------------------------------------------------------------");
             System.out.println("Total number of columns: " + lnRow);
             System.out.println("--------------------------------------------------------------------");
 
-            for (int lnCtr = 1; lnCtr <= lnRow; lnCtr++){
+            for (int lnCtr = 1; lnCtr <= lnRow; lnCtr++) {
                 System.out.println("Column index: " + (lnCtr) + " --> Label: " + poEntity.getMetaData().getColumnLabel(lnCtr));
-                if (poEntity.getMetaData().getColumnType(lnCtr) == Types.CHAR ||
-                    poEntity.getMetaData().getColumnType(lnCtr) == Types.VARCHAR){
+                if (poEntity.getMetaData().getColumnType(lnCtr) == Types.CHAR
+                        || poEntity.getMetaData().getColumnType(lnCtr) == Types.VARCHAR) {
 
                     System.out.println("Column index: " + (lnCtr) + " --> Size: " + poEntity.getMetaData().getColumnDisplaySize(lnCtr));
                 }
             }
         } catch (SQLException e) {
         }
-        
+
     }
-    
+
     /**
      * Description: Sets the sEmployID of this record.
-     * 
-     * @param fsValue 
+     *
+     * @param fsValue
      * @return result as success/failed
      */
-    public JSONObject setEmployerID(String fsValue){
+    public JSONObject setEmployerID(String fsValue) {
         return setValue("sEmployID", fsValue);
     }
-    
+
     /**
      * @return The sEmployID of this record.
      */
-    public String getEmployerID(){
+    public String getEmployerID() {
         return (String) getValue("sEmployID");
     }
-    
+
     /**
      * Description: Sets the sBranchCd of this record.
-     * 
-     * @param fsValue 
+     *
+     * @param fsValue
      * @return result as success/failed
      */
-    public JSONObject setBranchCode(String fsValue){
+    public JSONObject setBranchCode(String fsValue) {
         return setValue("sBranchCd", fsValue);
     }
-    
+
     /**
-     * @return The sBranchCd of this record. 
+     * @return The sBranchCd of this record.
      */
-    public String getBranchCode(){
+    public String getBranchCode() {
         return (String) getValue("sBranchCd");
     }
-    
+
     /**
      * Description: Sets the sLastName of this record.
-     * 
-     * @param fsValue 
+     *
+     * @param fsValue
      * @return result as success/failed
      */
-    public JSONObject setLastName(String fsValue){
+    public JSONObject setLastName(String fsValue) {
         return setValue("sLastName", fsValue);
     }
-    
+
     /**
-     * @return The sLastName of this record. 
+     * @return The sLastName of this record.
      */
-    public String getLastName(){
+    public String getLastName() {
         return (String) getValue("sLastName");
     }
-    
+
     /**
      * Description: Sets the sFrstName of this record.
-     * 
-     * @param fsValue 
+     *
+     * @param fsValue
      * @return result as success/failed
      */
-    public JSONObject setFristName(String fsValue){
+    public JSONObject setFristName(String fsValue) {
         return setValue("sFrstName", fsValue);
     }
-    
+
     /**
-     * @return The sFrstName of this record. 
+     * @return The sFrstName of this record.
      */
-    public String getFristName(){
+    public String getFristName() {
         return (String) getValue("sFrstName");
     }
-    
+
     /**
      * Description: Sets the sMiddName of this record.
-     * 
-     * @param fsValue 
+     *
+     * @param fsValue
      * @return result as success/failed
      */
-    public JSONObject setMiddleName(String fsValue){
+    public JSONObject setMiddleName(String fsValue) {
         return setValue("sMiddName", fsValue);
     }
-    
+
     /**
-     * @return The sMiddName of this record. 
+     * @return The sMiddName of this record.
      */
-    public String getMiddleName(){
+    public String getMiddleName() {
         return (String) getValue("sMiddName");
     }
-    
+
     /**
      * Description: Sets the cRecdStat of this record.
-     * 
-     * @param fsValue 
+     *
+     * @param fsValue
      * @return result as success/failed
      */
-    public JSONObject setRecdStat(String fsValue){
+    public JSONObject setRecdStat(String fsValue) {
         return setValue("cRecdStat", fsValue);
     }
-    
+
     /**
-     * @return The cRecdStat of this record. 
+     * @return The cRecdStat of this record.
      */
-    public String getRecdStat(){
+    public String getRecdStat() {
         return (String) getValue("cRecdStat");
     }
 
     /**
      * Sets record as active.
-     * 
+     *
      * @param fbValue
      * @return result as success/failed
      */
-    public JSONObject setActive(boolean fbValue){
+    public JSONObject setActive(boolean fbValue) {
         return setValue("cRecdStat", fbValue ? "1" : "0");
     }
-    
+
     /**
-     * @return If record is active. 
+     * @return If record is active.
      */
-    public boolean isActive(){
+    public boolean isActive() {
         return ((String) getValue("cRecdStat")).equals("1");
     }
-    
+
     /**
      * Sets the user encoded/updated the record.
-     * 
-     * @param fsValue 
+     *
+     * @param fsValue
      * @return result as success/failed
      */
-    public JSONObject setModifiedBy(String fsValue){
+    public JSONObject setModifiedBy(String fsValue) {
         return setValue("sModified", fsValue);
     }
-    
+
     /**
-     * @return The user encoded/updated the record 
+     * @return The user encoded/updated the record
      */
-    public String getModifiedBy(){
+    public String getModifiedBy() {
         return (String) getValue("sModified");
     }
-    
+
     /**
      * Sets the date and time the record was modified.
-     * 
-     * @param fdValue 
+     *
+     * @param fdValue
      * @return result as success/failed
      */
-    public JSONObject setModifiedDate(Date fdValue){
+    public JSONObject setModifiedDate(Date fdValue) {
         return setValue("dModified", fdValue);
     }
-    
+
     /**
      * @return The date and time the record was modified.
      */
-    public Date getModifiedDate(){
+    public Date getModifiedDate() {
         return (Date) getValue("dModified");
     }
-    
+
     /**
      * Gets the SQL statement for this entity.
-     * 
+     *
      * @return SQL Statement
      */
-    public String makeSQL(){
+    public String makeSQL() {
         return MiscUtil.makeSQL(this, "xClientNm»xBranchNm");
     }
-    
-    private void initialize(){
+
+    /**
+     * Gets the SQL statement for this entity.
+     *
+     * @return SQL Statement
+     */
+    public String makeSelectSQL() {
+        return MiscUtil.makeSQL(this, "xClientNm»xBranchNm");
+    }
+
+    private void initialize() {
         try {
             poEntity = MiscUtil.xml2ResultSet(System.getProperty("sys.default.path.metadata") + XML, getTable());
-            
+
             poEntity.last();
             poEntity.moveToInsertRow();
 
-            MiscUtil.initRowSet(poEntity);      
+            MiscUtil.initRowSet(poEntity);
             poEntity.updateString("cRecdStat", RecordStatus.ACTIVE);
-            
+
             poEntity.insertRow();
             poEntity.moveToCurrentRow();
 
             poEntity.absolute(1);
-            
+
             pnEditMode = EditMode.UNKNOWN;
         } catch (SQLException e) {
             e.printStackTrace();
             System.exit(1);
         }
-    } 
+    }
 }
